0e1046b97d61dee6f00d8cbff10510850599a4ff
weite122 <949193531@qq.com>
2020-05-23 01:02:52 +0800

    [A]添加icon

---
+++ src/components/Nav.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import React from 'react';
+
+const NavWrapper = styled.nav`
+  border: 1px solid blue;
+  > ul {
+    display:flex;
+    > li{
+      width: 33.3333%;
+      text-align:center;
+      padding: 16px;
+    }
+  }
+`;
+
+const Nav = () => {
+  return (
+    <NavWrapper>
+      <ul>
+        <li>
+          <Link to="/tags">标签页</Link>
+        </li>
+        <li>
+          <Link to="/money">记账页</Link>
+        </li>
+        <li>
+          <Link to="/statistics">统计页</Link>
+        </li>
+      </ul>
+    </NavWrapper>
+  );
+};
+
+export default Nav;
