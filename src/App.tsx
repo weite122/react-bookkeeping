584668283c5fa28c85e57051cc4b021dd19bd6ab
weite122 <949193531@qq.com>
2020-05-28 00:48:02 +0800

    [A]封装 findTag和useParams获取路由的id

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           <Route exact path="/tags">
             <Tags/>
           </Route>
-          <Route exact path="/tags/:tag">
+          <Route exact path="/tags/:id">
             <Tag/>
           </Route>
           <Route path="/money">
