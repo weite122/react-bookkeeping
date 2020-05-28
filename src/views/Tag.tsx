198cfe47049ce5a3bd47c408b30d0e4b63ee1aad
weite122 <949193531@qq.com>
2020-05-28 21:46:33 +0800

    [A]编辑tag名

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -28,9 +28,9 @@ const InputWrapper = styled.div`
 `;
 
 const Tag: React.FC = () => {
-  const {findTag} = useTags();
-  let {id} = useParams<Params>();
-  const tag = findTag(parseInt(id));
+  const {findTag, updateTag} = useTags();
+  let {id: idString} = useParams<Params>();
+  const tag = findTag(parseInt(idString));
   return (
     <Layout>
       <TopBar>
@@ -39,7 +39,10 @@ const Tag: React.FC = () => {
         <Icon/>
       </TopBar>
       <InputWrapper>
-        <Input label="标签名" type="text" placeholder="标签名" value={tag.name}/>
+        <Input label="标签名" type="text" placeholder="标签名" value={tag.name}
+               onChange={(e) => {
+                 updateTag(tag.id, {name: e.target.value});
+               }}/>
       </InputWrapper>
       <Center>
         <Space/>
