e155a49ae2eff7cd6db1a787b10cfb40ebb4d422
weite122 <949193531@qq.com>
2020-05-31 17:19:03 +0800

    [A]tags 数据可持续化

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import React from 'react';
 import {useTags} from 'useTags';
-import {createId} from 'lib/createId';
 
 
 const Wrapper = styled.section`
@@ -31,14 +30,9 @@ type Props = {
   onChange: (selected: number[]) => void;
 }
 const TagsSection: React.FC<Props> = (props) => {
-  const {tags, setTags} = useTags();
+  const {tags, addTag} = useTags();
   const selectedTagIds = props.value;
-  const onAddTag = () => {
-    const tagName = window.prompt('新标签的名称为');
-    if (tagName !== null) {
-      setTags([...tags, {id: createId(), name: tagName}]);
-    }
-  };
+
   const onToggleTag = (tagId: number) => {
     const index = selectedTagIds.indexOf(tagId);
     if (index >= 0) {
@@ -58,7 +52,10 @@ const TagsSection: React.FC<Props> = (props) => {
           >{tag.name}</li>
         )}
       </ol>
-      <button onClick={onAddTag}>新增标签</button>
+      <button onClick={() => {
+        addTag();
+      }}>新增标签2
+      </button>
     </Wrapper>
   );
 };
